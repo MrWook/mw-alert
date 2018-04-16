2b22019d3d017a57457ef7ac61967cf74071d215
MrWook <mr.wook@web.de>
2018-04-16 19:08:45 +0200

    - fixed uglify bug

--- src/modules/mw-alert-service.js
+++ src/modules/mw-alert-service.js
@@ -4,12 +4,11 @@ angular.module('mw-alert').service('mwAlertService', ['$rootScope', '$document',
 	//I'm totally aware of the fact that you shouldn't use DOM manipulation outside of a directive but this is for a greater GOOD!
 	let parent = angular.element($document[0].querySelector(parent_selector));
 	$timeout(function(){
-		angular.element(parent).injector().invoke(function($compile){
-			let $scope = angular.element(parent).scope();
-			parent.prepend($compile('<mw-alert></mw-alert>')($scope));
-			// Finally, refresh the watch expressions in the new element
-			$scope.$apply();
-		});
+		//create new scope object for the compile directive
+		let $scope = $rootScope.$new();
+		parent.prepend($compile('<mw-alert></mw-alert>')($scope));
+		//the $timeout service call $digest by itself so its not necessary at this point
+		// $scope.$digest();
 	}, 0);
 
 	let message;

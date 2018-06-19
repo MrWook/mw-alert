7fe7c0a7a7713abca7b1037d60d196410f886b58
MrWook <mr.wook@web.de>
2018-06-19 18:01:32 +0200

    - split tasks

--- gruntFile.js
+++ gruntFile.js
@@ -1,12 +1,16 @@
 'use strict';
 module.exports = function(grunt){
+	let name = '<%= pkg.name %>';
 
-	let copyright = '/**\n'+
-					' * @version v<%= pkg.version %>\n'+
-					' * @link https://github.com/MrWook/mw-alert\n'+
-					' * @license MIT\n'+
-					' * Copyright (c) '+(new Date()).getFullYear()+' MrWook\n'+
-					' */\n\r';
+	let copyright = `/**
+* @version v<%= pkg.version %>
+* @link <%= pkg.homepage %>
+* @license <%= pkg.license %>
+* Copyright (c) ${(new Date()).getFullYear()} <%= pkg.author %>
+*/\r`;
+
+	let prefix = '(function(angular, undefined){\n \'use strict\';\n';
+	let suffix = '\n})(angular);';
 
 	// Load all grunt tasks automatically
 	require('load-grunt-tasks')(grunt);
@@ -17,126 +21,18 @@ module.exports = function(grunt){
 	//Configure grunt
 	grunt.initConfig({
 						 pkg:        grunt.file.readJSON('package.json'),
-						 sass:       {
-							 dist: {
-								 files: [
-									 {
-										 expand: true,
-										 cwd:    'src/sass/',
-										 src:    ['**/*.scss'],
-										 dest:   'tmp/css/',
-										 ext:    '.css'
-									 }
-								 ]
-							 }
-						 },
-						 minifyHtml: {
-							 options: {
-								 cdata:  false,
-								 empty:  false,
-								 quotes: true
-							 },
-							 dist:    {
-								 files: [
-									 {
-										 expand: true,
-										 cwd:    'src/templates',
-										 src:    ['**/*.html'],
-										 dest:   'tmp/templates_min/'
-									 }
-								 ]
-							 }
-						 },
-						 html2js:    {
-							 dist:    {
-								 options: {
-									 base:         'tmp/templates_min',
-									 module:       'mw-alert-template',
-								 },
-								 src:  ['tmp/templates_min/**/*.html'],
-								 dest: 'tmp/templates.js'
-							 },
-							 options: {
-								 indentString: '	',
-								 singleModule: true
-							 }
-						 },
-						 babel: {
-							 options: {
-								 sourceMap: true,
-								 presets: ['env']
-							 },
-							 dist: {
-								 files: [
-									 {
-										 expand: true,
-										 cwd: 'src/modules/',
-										 src: ['**/*.js'],
-										 dest: 'tmp/modules',
-									 }
-								 ]
-							 }
-						 },
-						 concat:     {
-							 options: {
-								 sourceMap: true,
-								 banner:    copyright,
-							 },
-							 modules: {
-								 options: {
-									 banner:    copyright+"'use strict';\n",
-									 process: function(src, filepath) {
-										 return src.replace(/(^|\n)[ \t]*('use strict'|"use strict");?\s*/g, '$1');
-									 },
-								 },
-								 src:  [
-									 'src/extra/.prefix',
-									 'src/mw-alert.js',
-									 'tmp/modules/**/*.js',
-									 'tmp/templates.js',
-									 'src/extra/.suffix'
-								 ],
-								 dest: 'dist/js/mw-alert.js'
-							 },
-							 css:     {
-								 src:  ['tmp/css/**/*.css'],
-								 dest: 'dist/css/mw-alert.css'
-							 }
-						 },
-						 uglify: {
-							 build: {
-								 options: {
-									 sourceMap: true,
-									 banner:    copyright,
-								 },
-								 files:[
-									 {
-										 src: ['dist/js/mw-alert.js'],
-										 dest: 'dist/js/mw-alert.min.js'
-									 }
-								 ]
-							 }
-						 },
-						 cssmin: {
-							 build: {
-								 options: {
-									 sourceMap: true,
-									 banner:    copyright,
-								 },
-								 files:[
-									 {
-										 src: ['dist/css/mw-alert.css'],
-										 dest: 'dist/css/mw-alert.min.css'
-									 }
-								 ]
-							 }
-						 },
-						 clean:      ['tmp/'],
+						 babel:      require('./grunt/babel')(),
+						 clean:      require('./grunt/clean')(),
+						 concat:     require('./grunt/concat')(copyright, name, prefix, suffix),
+						 cssmin:     require('./grunt/cssmin')(copyright, name),
+						 html2js:    require('./grunt/html2js')(name),
+						 minifyHtml: require('./grunt/minifyHtml')(),
+						 sass:       require('./grunt/sass')(),
+						 uglify:     require('./grunt/uglify')(copyright, name)
 					 });
 
-
 	// Build distribution files
-	grunt.registerTask('build', [
+	grunt.registerTask('default', [
 		'sass',
 		'minifyHtml',
 		'html2js',
